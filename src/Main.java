868ae0090075561e4704fb9cd977333beeb145c3
its-thazbeeha <thazb2023@gmail.com>
2026-04-23 19:32:02 +0530

    Upload complete Lab Worksheet 1

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,13 @@
-import javax.swing.JFrame;
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+void main() {
+    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+    // to see how IntelliJ IDEA suggests fixing it.
+    IO.println(String.format("Hello and welcome!"));
 
-public class Main {
-    public static void main(String[] args) {
-        JFrame frame = new JFrame();
-
-        frame.setSize(800, 600);
-        frame.setTitle("Welcome to Java");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
+    for (int i = 1; i <= 5; i++) {
+        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+        IO.println("i = " + i);
     }
 }
---
+++ src/Q1/Q1.java
@@ -0,0 +1,15 @@
+package Q1;
+
+import javax.swing.JFrame;
+
+public class Q1 {
+    public static void main(String[] args) {
+
+        JFrame frame = new JFrame();
+
+        frame.setSize(800, 600);
+        frame.setTitle("Welcome to Java");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
